fec84ef9c15fea821b63c3ca30225744bb617313
mebenal <willtreatyaraluen@gmail.com>
2021-11-15 14:44:28 -0700

    tasks update

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -4,16 +4,26 @@
 //  "taskCategory" = ""
 //  "taskCompletionStatus" = False
 //  "" time (possibly)
-
+const Task = require('../models/task')
 
 exports.getDailyTasks = (req, res, next) => { // gets all page data and users tasks
+  const now = new Date();
+  const today = new Date(now.getFullYear()+'-'+(now.getMonth()+1)+'-'+now.getDate());
+  const tomorrow = new Date(now.getFullYear()+'-'+(now.getMonth()+1)+'-'+(now.getDate()+1))
+  console.log(today)
+  console.log(tomorrow)
+  console.log(user)
+  Task.getTasks(today, tomorrow, req.user)
+  .then(tasks => {
     //return res.status(422).send({
-      return res.send({
+    return res.send({
       path: '/daily-tasks',
       pageTitle: 'Daily Tasks',
       date: 'date here',
       task: 'array of tasks', // has name and category color - Sam
     });
+  })
+  
   };
   
   exports.postAddNewTask = (req, res, next) => { // Creates a new task
