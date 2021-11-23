a016091f63657218fc89646a44824177b28e1c3c
mebenal <willtreatyaraluen@gmail.com>
2021-11-23 14:26:54 -0700

    headers

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 
 app.use((req, res, next) => {
-  console.log(req.rawHeaders);
+  console.log(req.headers);
   console.log(req.body);
   next();
 })
