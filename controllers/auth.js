ac2b21b9f90cf62f62bf017ee7b795ffe2bafb74
mebenal <willtreatyaraluen@gmail.com>
2021-12-02 10:25:27 -0700

    check login status

--- controllers/auth.js
+++ controllers/auth.js
@@ -206,4 +206,8 @@ exports.postNewPassword = (req, res, next) => {
       error.httpStatusCode = 500;
       return next(error);
     });
+
+  exports.getLoggedIn = (req, res, next) => {
+    res.json({loggedIn:req.session.isLoggedIn});
+  }
 };
