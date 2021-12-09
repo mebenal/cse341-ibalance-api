c0c2b45e81a6a6d046894e9baa74c2518e492e8c
mebenal <willtreatyaraluen@gmail.com>
2021-12-08 20:58:36 -0700

    log sockets

--- app.js
+++ app.js
@@ -146,7 +146,7 @@ mongoose
         User.find({ email: data }).then(users => {
           if (users.length) {
             io.of('/').sockets.forEach(data => {
-              console.log(data.nickname);
+              console.log(data.socket);
             });
             console.log(socket.nickname)
             Message.getMessages(socket.nickname, users[0].email).then(
